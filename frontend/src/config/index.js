a916175de44793f358649a0b03e01c2fed317536
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-02-15 09:50:09 +0530

    FEAT : Creation of custom form. Created Register and login using the custom form we created.

---
+++ frontend/src/config/index.js
@@ -0,0 +1,42 @@
+export const registerFormControls = [
+    {
+        name: 'userName',
+        label: 'User Name',
+        placeholde: "Enter your user name",
+        componentType: "input",
+        type: "text"
+    },
+    {
+        name: 'email',
+        label: 'Email',
+        placeholde: "Enter your email",
+        componentType: "input",
+        type: "text"
+    }
+    ,
+    {
+        name: 'password',
+        label: 'Password',
+        placeholde: "Enter your password",
+        componentType: "input",
+        type: "password"
+    }
+]
+
+export const loginFormControls = [
+    {
+        name: 'email',
+        label: 'Email',
+        placeholde: "Enter your email",
+        componentType: "input",
+        type: "text"
+    }
+    ,
+    {
+        name: 'password',
+        label: 'Password',
+        placeholde: "Enter your password",
+        componentType: "input",
+        type: "password"
+    }
+]
--- frontend/src/pages/auth/register.jsx
+++ frontend/src/pages/auth/register.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import CommonForm from '@/components/common/form'
+import { registerFormControls } from '@/config'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const initialState = {
+    userName: '',
+    email: "",
+    password: ""
+}
 
 const AuthRegister = () => {
+
+    const [formData, setFormData] = useState(initialState)
+
+    function onSubmit() {
+
+    }
+
     return (
-        <div>register</div>
+        <div className='mx-auto w-full max-w-md space-y-6'>
+            <div className='text-center'>
+                <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create new account</h1>
+                <p className='mt-2'>Already have an account
+                    <Link className='font-medium text-primary ml-2 hover:underline' to='/auth/login'>Login</Link>
+                </p>
+            </div>
+            <CommonForm
+                formControls={registerFormControls}
+                buttonText={'Sign Up'}
+                formData={formData}
+                setFormData={setFormData}
+                onSubmit={onSubmit}
+            />
+        </div>
     )
 }
 
